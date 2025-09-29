c872b3030db626df6343022946812ec7514a08e2
hamadismail <hamad.ismail.gub@gmail.com>
2025-09-29 12:42:10 +0800

    setup backend with prisma and seedAdmin

---
+++ server/src/utils/seedAdmin.ts
@@ -0,0 +1,33 @@
+import bcryptjs from "bcryptjs";
+import { prisma } from "../config/db";
+
+export const seedSuperAdmin = async () => {
+  try {
+    const isAdminExist = await prisma.user.findUnique({
+      where: { email: "admin@gmail.com" },
+    });
+
+    if (isAdminExist) {
+      console.log("Super Admin Already Exists!");
+      return;
+    }
+
+    console.log("Trying to create Super Admin...");
+
+    const hashedPassword = await bcryptjs.hash("admin@password", Number(10));
+
+    const payload = {
+      name: "Admin",
+      email: "admin@gmail.com",
+      password: hashedPassword,
+    };
+
+    await prisma.user.create({
+      data: payload,
+    });
+
+    console.log("Super Admin Created Successfuly! \n");
+  } catch (error) {
+    console.log(error);
+  }
+};
